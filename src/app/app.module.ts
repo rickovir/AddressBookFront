8a44127e71d092895173344d72ecc4484e4fdc0c
Ricko Virnanda <rickovir@gmail.com>
2018-06-06 04:31:24 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './layout/main/main.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+
+
+const appRoute: Routes = [
+  {
+    path: '',
+    component:MainComponent,
+    children:[{
+      path : '',
+      component:HomeComponent
+    }]
+  },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoute,
+      { enableTracing: true }
+    ),
     BrowserModule
   ],
   providers: [],
